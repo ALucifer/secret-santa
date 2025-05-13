cd3682d6c6d43dd0be9e5cd496c7e66a59a31562
acoubard <coubard.alexis72@gmail.com>
2025-05-13 21:23:04 +0200

    handle wrong image in gift url

--- assets/vue/components/Wish/WishGift.vue
+++ assets/vue/components/Wish/WishGift.vue
@@ -1,7 +1,11 @@
 <template>
   <WishContainer content-container-css-classes="flex-1 flex flex-col justify-between overflow-hidden">
     <template #icon>
-      <img v-if="image" :src="image">
+      <img
+        v-if="image"
+        :src="image"
+        @error="onError"
+      >
       <GiftIcon v-else />
     </template>
     <template #content>
@@ -16,6 +20,13 @@
 <script setup lang="ts">
 import WishContainer from "@app/components/Wish/WishContainer.vue";
 import GiftIcon from "@app/icons/GiftIcon.vue";
+import {ref} from "vue";
 
-defineProps<{ title: string, url: string, image?: string }>()
+const props = defineProps<{ title: string, url: string, image?: string }>()
+
+const image = ref(props.image)
+
+function onError() {
+  image.value = null
+}
 </script>
